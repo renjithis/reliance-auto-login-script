736f5a30801866db0123c7814776619aad942c6b
Renjith I S <mailrenjithis@gmail.com>
2013-07-01 23:15:24 +0530

    Reordered the functions a bit, made a main() function, made constants global, changed urls and changed internet on test

--- reliance-login.py
+++ reliance-login.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # encoding: utf-8
 """
 # Reliance Login Script for Python 2.x v1.0
@@ -16,13 +16,28 @@
 # GNU General Public License for more details.
 """
  
-import urllib2, urllib, cookielib, time, re, sys
+import urllib2
+import urllib
+import cookielib
+import time
+import re
+import sys
+#import argparse - TODO : use argparse to handle arguments, take username and password as agruments
 
-username = 'username' 
+# Default value definitions
+username = 'username'
 password = 'password'
 
+# Constant definitions
+internet_on_test_url='http://google.com'
+internet_off_test_string='reliance'
+reliance_start_portal_url="http://reliancebroadband.co.in/reliance/startportal_isg.do"
+reliance_login_url='http://reliancebroadband.co.in/reliance/login.do'
+reliance_logout_url='http://reliancebroadband.co.in/reliance/logout.do'
+user_agent_string='Mozilla/5.0 (Windows NT 6.1; U; ru; rv:5.0.1.6) Gecko/20110501 Firefox/5.0.1 Firefox/5.0.1'
+
 '''You don't normally have to edit anything below this line'''
-debug = False
+debug = True
 check_interval = 600
 
 if ((len(sys.argv) > 2) and (sys.argv[2] == '-d')): debug = True
@@ -30,8 +45,9 @@ if ((len(sys.argv) > 2) and (sys.argv[2] == '-d')): debug = True
 def get_url(url, data=None, timeout=60, opener=None):
   '''get_url accepts a URL string and return the server response code, response headers, and contents of the file'''
   '''ref: http://pythonfilter.com/blog/changing-or-spoofing-your-user-agent-python.html'''
+  if debug: print "get_url:",url,"data:",data,"opener:",opener
   req_headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; U; ru; rv:5.0.1.6) Gecko/20110501 Firefox/5.0.1 Firefox/5.0.1'
+    'User-Agent': user_agent_string
   }
   request = urllib2.Request(url, headers=req_headers)
   if not opener:
@@ -47,12 +63,12 @@ def is_internet_on():
   '''test if the machine is connected to the internet'''
   if debug: print "Testing"
   try:
-    code, headers, html, opener = get_url('http://74.125.113.99', timeout=10)
+    code, headers, html, opener = get_url(internet_on_test_url, timeout=10)
     if debug: print html
-    if re.search('google.com', html):
-      return True
-    else:
+    if re.search(internet_off_test_string, html):
       return False
+    else:
+      return True
   except: 
     if debug: print "Error"
     return False
@@ -60,25 +76,25 @@ def is_internet_on():
 
 def internet_connect():
   '''try to connect to the internet'''
-  code, headers, html, cur_opener = get_url("http://10.239.89.15/reliance/startportal_isg.do", timeout=10)
+  code, headers, html, cur_opener = get_url(reliance_start_portal_url, timeout=10)	# Why are we doing this?
   if debug: print html
   login_data = urllib.urlencode({'userId' : username, 'password' : password, 'action' : 'doLoginSubmit'})
-  code, headers, html, cur_opener = get_url('http://10.239.89.15/reliance/login.do', data=login_data, opener=cur_opener)
+  code, headers, html, cur_opener = get_url(reliance_login_url, data=login_data, opener=cur_opener)
   if debug: print html
 
 def internet_disconnect():
   '''try to disconnect from the internet'''
-  code, headers, html, cur_opener = get_url('http://10.239.89.15/reliance/login.do', timeout=10)
+  code, headers, html, cur_opener = get_url(reliance_login_url, timeout=10)
   if debug: print html
-  code, headers, html, cur_opener = get_url('http://10.239.89.15/reliance/logout.do', opener=cur_opener)
+  code, headers, html, cur_opener = get_url(reliance_logout_url, opener=cur_opener)
   if debug: print html
 
 def internet_keep_alive():
   '''login and keep the connection live'''
   while True:
     if not is_internet_on():
-      internet_connect()
       if debug: print "Not connected"
+      internet_connect()
     else:
       if debug: print "Connected"
       pass
@@ -90,24 +106,26 @@ def print_usage():
   print "usage:" + sys.argv[0] + " [login|logout]\n" 
   print "If there are no arguments it runs in an infinite loop and will try to remain connected to the internet."
 
-keep_alive = True
-if (len(sys.argv) > 1):
-  op = sys.argv[1]
-  if op == 'login':
-    internet_connect()
-    keep_alive = False
-  elif op == 'logout':
-    internet_disconnect()
-    keep_alive = False
-  elif op == 'keep-alive':
-    keep_alive = True
-    pass
-  else:
-    print_usage()
-    keep_alive = False
+def main():
+  keep_alive = True
+  if (len(sys.argv) > 1):
+    op = sys.argv[1]
+    if op == 'login':
+      internet_connect()
+      keep_alive = False
+    elif op == 'logout':
+      internet_disconnect()
+      keep_alive = False
+    elif op == 'keep-alive':
+      keep_alive = True
+      pass
+    else:
+      print_usage()
+      keep_alive = False
+  
+  ''' default action without any arguments - keep alive'''
+  if keep_alive:
+    internet_keep_alive();
 
-''' default action without any arguments - keep alive'''
-if keep_alive:
-  internet_keep_alive();
-
-
+if __name__ == '__main__':
+  main()
